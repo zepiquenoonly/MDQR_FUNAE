85a3c51d1186a84f428f5610bf9ae97824761c10
edsonjorgef1 <Edsonjorgef1@gmail.com>
2025-11-18 18:03:06 +0200

    feat(emails): criar templates para notificação de novo comentário
    - Template HTML para alertar sobre comentários públicos
    - Versão texto plano
    - Mostrar autor e data do comentário
    - Exibir conteúdo do comentário ou descrição da atualização
    - Design limpo focado no conteúdo da mensagem
    - Link para ver todas as atualizações

---
+++ resources/views/emails/grievances/comment-added-text.blade.php
@@ -0,0 +1,28 @@
+Nova Atualização
+================
+
+Prezado(a),
+
+Foi adicionado um novo comentário à sua reclamação.
+
+Número de Referência: {{ $grievance->reference_number }}
+
+{{ $update->user ? $update->user->name : 'Sistema' }} • {{ $update->created_at->format('d/m/Y H:i') }}
+
+@if($update->comment)
+{{ $update->comment }}
+@else
+{{ $update->description }}
+@endif
+
+Para ver todas as atualizações, aceda a:
+{{ route('grievance.track') }}?ref={{ $grievance->reference_number }}
+
+Continue a acompanhar o progresso da sua reclamação através do link acima.
+
+Atenciosamente,
+Equipa FUNAE
+
+---
+Esta é uma mensagem automática. Por favor não responda a este email.
+© {{ date('Y') }} FUNAE - Fundo Nacional de Energia
